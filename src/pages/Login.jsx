ccb33809c62cdbe69762d08a7f828caaf58eebdd
hari balaji <145361290+haribalji@users.noreply.github.com>
2025-05-03 08:23:34 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,8 +10,8 @@ import { userExists } from '../redux/reducers/auth';
 import toast from 'react-hot-toast';
 // import { server } from '../constants/config';
 import axios from 'axios';
-// import { server } from "./constants/config";
- const server ="https://chatapp-kchw.onrender.com";
+import { server } from "./constants/config";
+ // const server ="https://chatapp-kchw.onrender.com";
 function Login() {
     const [isLogin, setisLogin] = useState(true);
     const toggleLogin = () => setisLogin((prev)=>!prev);
