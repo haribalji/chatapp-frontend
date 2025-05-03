2322a629f6ee89bc32c0aa4532feaf1a00aba329
hari balaji <145361290+haribalji@users.noreply.github.com>
2025-05-03 08:35:11 +0530

    Update config.js

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,6 @@
 
 // export const server ="https://chatapp-kchw.onrender.com";
-export const server = import.meta.env.VITE_SERVER;
+// export const server = import.meta.env.VITE_SERVER;
 
+const server = import.meta.env.VITE_SERVER;
+export default server;
